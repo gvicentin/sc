1a33eb2a4038a830edcebb0528bb93d7be29c5d0
Guilherme Vicentin <gvicentin@ciandt.com>
2022-07-07 13:34:56 -0300

    Reading input

--- sc.c
+++ sc.c
@@ -7,7 +7,7 @@
 
 void printUsageAndExit(const char *binary);
 
-uint8_t octetFromStr(const char *str, size_t start, size_t end);
+uint8_t extractFromStr(const char *str, size_t start, size_t end);
 
 int main(int argc, char *argv[]) {
 
@@ -34,10 +34,12 @@ int main(int argc, char *argv[]) {
     printf("Input: %s\n", string);
 
     uint8_t fOctet, sOctet, tOctet, foOctet;
-    fOctet = octetFromStr(string, pmatch[1].rm_so, pmatch[1].rm_eo);
-    sOctet = octetFromStr(string, pmatch[2].rm_so, pmatch[2].rm_eo);
-    tOctet = octetFromStr(string, pmatch[3].rm_so, pmatch[3].rm_eo);
-    foOctet = octetFromStr(string, pmatch[4].rm_so, pmatch[4].rm_eo);
+    uint8_t decimalMask;
+    fOctet = extractFromStr(string, pmatch[1].rm_so, pmatch[1].rm_eo);
+    sOctet = extractFromStr(string, pmatch[2].rm_so, pmatch[2].rm_eo);
+    tOctet = extractFromStr(string, pmatch[3].rm_so, pmatch[3].rm_eo);
+    foOctet = extractFromStr(string, pmatch[4].rm_so, pmatch[4].rm_eo);
+    decimalMask = extractFromStr(string, pmatch[5].rm_so, pmatch[5].rm_eo);
 
     printf("Fist octet: %u\n", fOctet);
     printf("Second octet: %u\n", sOctet);
@@ -50,7 +52,11 @@ int main(int argc, char *argv[]) {
     ip |= tOctet << 8;
     ip |= foOctet;
 
+    uint32_t mask = 0xFFFFFFFF;
+    mask = mask << (32 - decimalMask);
+
     printf("IP Long: %u\n", ip);
+    printf("Mask Long: %u\n", mask);
 
     // de-initialization
     regfree(&reegex);
@@ -64,7 +70,7 @@ void printUsageAndExit(const char *binary) {
     exit(1);
 }
 
-uint8_t octetFromStr(const char *str, size_t start, size_t end) {
+uint8_t extractFromStr(const char *str, size_t start, size_t end) {
     char buff[OCTET_BUFF_LEN];
     int len = end - start;
 
