e043a396fd170ca2c0e033c9e29ea6b0d10c44dd
Guilherme Vicentin <gvicentin@ciandt.com>
2023-05-31 19:18:12 -0300

    Refactored miniunit and tests

--- tests/minunit.h
+++ tests/minunit.h
@@ -1,9 +1,34 @@
 #ifndef MINUNIT_H
 #define MINUNIT_H
 
-#define mu_assert(message, test) do { if (!(test)) return message; } while (0)
-#define mu_run_test(test) do { char *message = test(); g_testsRun++; \
-                                if (message) return message; } while (0)
+#define MU_PASS     return NULL
+#define MU_MSG_FMT  "[FAILED] %s | %s::%d -> "
+#define MU_MSG_ARGS __FILE__, __FUNCTION__, __LINE__
+
+#define MU_ASSERT(test, message)                                   \
+    do {                                                           \
+        if (!(test)) {                                             \
+            sprintf(g_assertMsg, MU_MSG_FMT message, MU_MSG_ARGS); \
+            return g_assertMsg;                                    \
+        }                                                          \
+    } while (0)
+
+#define MU_ASSERT_FMT(test, msg_format, ...)                                         \
+    do {                                                                             \
+        if (!(test)) {                                                               \
+            sprintf(g_assertMsg, MU_MSG_FMT msg_format, MU_MSG_ARGS, ##__VA_ARGS__); \
+            return g_assertMsg;                                                      \
+        }                                                                            \
+    } while (0)
+
+#define MU_TEST(test)                \
+    do {                             \
+        char *message = test();      \
+        g_testsRun++;                \
+        if (message) return message; \
+    } while (0)
+
+extern char g_assertMsg[1024];
 extern int g_testsRun;
 
-# endif // MINUNIT_H
+#endif  // MINUNIT_H
